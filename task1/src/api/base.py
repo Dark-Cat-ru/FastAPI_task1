9fa4b02a2a5887790f69b054113be99da95da6dd
Savinov Mikhail <m.savinov2006@yandex.ru>
2026-03-12 16:08:00 +0400

    доработанная первая версия пректа

--- task1/src/api/base.py
+++ task1/src/api/base.py
@@ -6,18 +6,16 @@ router = APIRouter()
 
 user_repo = [
     {
-        'id': 0,
         'login': 'cat', 
         'password': '666darkCat13!'
     },
     {
-        'id': 1,
         'login': 'dog',
         'password': '13BadDog666!'
     },
 ]
 
-@router.get("cats/", status_code=status.HTTP_200_OK)
+@router.get("/cats", status_code=status.HTTP_200_OK)
 async def get_cat_data(cat: Cat):
     response = {
         "name": cat.name,
@@ -27,27 +25,7 @@ async def get_cat_data(cat: Cat):
     }
     return response
 
-@router.post("change_user/<user_id>/", status_code=status.HTTP_200_OK, response_model=User)
-async def change_user_data(user: User, user_id: int):
-    if len(user.login) < 3:
-        raise HTTPException(
-            detail="Длина логина должна быть не меньше 3 символов",
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-        )
-    for i in user_repo:
-        if i['login'] == user.login:
-            responce = {
-                'id': user_id,
-                "login": user.login,
-                "password": user.password
-            }
-            return User.model_validate(obj=responce)
-        raise HTTPException(
-            detail="Не найден пользователь с таким id",
-            status_code=status.HTTP_404_NOT_FOUND,
-        )
-
-@router.put("create_user/", status_code=status.HTTP_201_CREATED, response_model=User)
+@router.post("/create_user", status_code=status.HTTP_201_CREATED, response_model=User)
 async def create_user(new_user: User):
     if len(new_user.login) < 3:
         raise HTTPException(
@@ -61,21 +39,39 @@ async def create_user(new_user: User):
         )
     else:
         responce = {
-            'id': len(user_repo),
             'login': new_user.login,
             'password': new_user.password,
         }
         return User.model_validate(obj=responce)
 
-@router.delete("delete_user/<user_id>/", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id : int):
+@router.put("/change_user/<user_login>", status_code=status.HTTP_200_OK, response_model=User)
+async def change_user_data(user: User, user_login: str):
+    if len(user.login) < 3:
+        raise HTTPException(
+            detail="Длина логина должна быть не меньше 3 символов",
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+        )
+    for i in user_repo:
+        if i['login'] == user_login:
+            responce = {
+                "login": user.login,
+                "password": user.password
+            }
+            return User.model_validate(obj=responce)
+        raise HTTPException(
+            detail="Не найден пользователь с таким login",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+
+@router.delete("/delete_user/<user_login>", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_login : int):
     if not any(user['login'] == user.login for user in user_repo):
         raise HTTPException(
-            detail="Пользователя с таким логином нет",
+            detail="Пользователя с таким id уже нет",
             status_code=status.HTTP_404_NOT_FOUND,
         )
     else:
         for u in user_repo:
-            if u["id"] == user_id:
-                user_repo = user_repo.pop(user_id)
+            if u["login"] == user_login:
+                user_repo = user_repo.pop(user_login)
                 return user_repo
--- task1/src/models/users.py
+++ task1/src/models/users.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, SecretStr, Field
 
 class User(BaseModel):
-    login: str = Field(max_length=12)
+    login: str = Field(max_length=12, min_length=5)
     password: SecretStr = Field(min_length=8)
